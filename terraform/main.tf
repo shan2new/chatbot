c6dea153854a957471cafe001e3fb6d40efcbd8d
Shantanu Sinha <shantanusinha95@gmail.com>
2024-06-20 05:15:42 +0530

    chore: update health check for frontend (infra)

--- terraform/main.tf
+++ terraform/main.tf
@@ -126,7 +126,7 @@ resource "aws_ecs_task_definition" "frontend" {
       }
       command = ["npm", "start"]
       healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:3000/health || exit 1"]
+        command     = ["CMD-SHELL", "curl -f http://localhost:3000 || exit 1"]
         interval    = 60
         timeout     = 5
         retries     = 3
